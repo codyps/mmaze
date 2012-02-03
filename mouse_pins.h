f661efd28e9607caaf6c1defefec9d4808c381a2
Cody Schafer <cpschafer@gmail.com>
2012-02-02 22:43:11 -0500

    add pin mapping doc for motor controllers.

---
+++ mouse_pins.h
@@ -0,0 +1,36 @@
+#ifndef MOUSE_PINS_H_
+#define MOUSE_PINS_H_
+
+/* DECAY0  = PB4
+ * DIR0    = PB3
+ * MODE0_2 = PB2
+ * MODE0_1 = PB1
+ * MODE0_0 = PB0
+ */
+
+/* nHOME0  PH4
+ * nENBL0  PH3
+ * nRESET0 PH2
+ * nSLEEP0 PH1
+ * nFAULT0 PH0
+ */
+
+/* DECAY1  PD4
+ * DIR1    PD3
+ * MODE1_2 PD2
+ * MODE1_1 PD1
+ * MODE1_0 PD0
+ */
+
+/* nHOME1  PG4
+ * nENBL1  PG3
+ * nRESET1 PG2
+ * nSLEEP1 PG1
+ * nFAULT1 PG0
+ */
+
+/* STEP0 PF6/CCP1
+ * STEP1 PF5/CCP2
+ */
+
+#endif
