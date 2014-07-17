909745a68dac631d07c9177035ffb66c334e443a
Cody P Schafer <devel@codyps.com>
2014-07-16 23:13:41 -0700

    tweak

--- init_vector.c
+++ init_vector.c
@@ -1,11 +1,11 @@
-
 __attribute__((weak,interrupt,noreturn))
-void __bad_interrupt(void)
+void bad_interrupt(void)
 {
 	__builtin_trap();
 }
 
-#define VECTOR(n) __attribute__((weak,interrupt,alias("__bad_interrupt"))) void _isr_##n(void);
+#define IS_reset_EQ_reset 1
+#define VECTOR(n) __attribute__((weak,interrupt,alias("bad_interrupt"))) void _isr_##n(void);
 #define VECTOR_NULL
 #include "vectors.def"
 #undef VECTOR
@@ -17,11 +17,9 @@ extern void __stack_high;
 __attribute__ ((section(".vector")))
 void (* const __vectors[])(void) = {
 	&__stack_high,
-
 #define VECTOR(n) _isr_##n,
 #define VECTOR_NULL NULL
 #include "vectors.def"
 #undef VECTOR
 #undef VECTOR_NULL
 };
-
