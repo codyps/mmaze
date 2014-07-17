ece97705bcb24c6990315fa277c28147c1d2815b
Cody P Schafer <devel@codyps.com>
2014-07-16 22:21:29 -0700

    poke

--- init.c
+++ init.c
@@ -8,23 +8,37 @@
 __attribute__((noreturn))
 extern void main (void);
 
-extern uint32_t __bss_start__, __bss_end__, __data_start__, __data_end__, __data_load_start__;
+extern uint32_t __bss_start[], __bss_end[], __data_start[], __data_end[], __data_load_start[];
 
-__attribute__((externally_visible,noreturn,interrupt,section(".init0")))
+typedef void (*init_fn)(void);
+extern init_fn __preinit_array_start[], __preinit_array_end[], __init_array_start[], __init_array_end[];
+
+__attribute__((noreturn,interrupt))
 void __init(void)
 {
 	uint32_t *s, *d;
+	init_fn *f;
 
-	if (&__data_start__ != &__data_load_start__) {
-		s = &__data_load_start__;
-		d = &__data_start__;
-		while (d < &__data_end__)
-			*d++ = *s++;
+	s = __data_load_start;
+	d = __data_start;
+	while (d < __data_end)
+		*d++ = *s++;
+
+	d = __bss_start;
+	while (d < __bss_end)
+		*d++ = 0;
+
+	f = __preinit_array_start;
+	while (f < __preinit_array_end) {
+		(*f)();
+		f++;
 	}
 
-	d = &__bss_start__;
-	while (d < &__bss_end__)
-		*d++ = 0;
+	f = __init_array_start;
+	while (f < __init_array_end) {
+		(*f)();
+		f++;
+	}
 
 	main();
 }
