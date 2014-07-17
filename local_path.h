be8d3354b6b32469f014cacace82402aa3555072
Cody P Schafer <devel@codyps.com>
2014-07-16 22:27:35 -0700

    notes

--- local_path.h
+++ local_path.h
@@ -3,6 +3,12 @@
 
 /* Functions for planning the motor settings to get us from point A to point B,
  * where A and B are close together
+ *
+ * Take an input direction and velocity, and an output direction and offset.
+ * Attempt to match it.
+ *
+ * Draw an arc from our location to the goal. Calculate motor speeds based on
+ * that arch and the wheel base.
  */
 
 #endif
