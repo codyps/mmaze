a2bae8968a3793710a51cbb3b3aba5e529b490e4
Cody P Schafer <devel@codyps.com>
2014-07-16 22:24:37 -0700

    poke

--- crt0.c
+++ crt0.c
@@ -5,19 +5,18 @@ void __bad_interrupt(void)
 	__builtin_trap();
 }
 
-#define VECTOR(n) \
-	__attribute__((weak,interrupt,alias("__bad_interrupt"))) void _isr_##n(void);
+#define VECTOR(n) __attribute__((weak,interrupt,alias("__bad_interrupt"))) void _isr_##n(void);
 #define VECTOR_NULL
 #include "vectors.def"
 #undef VECTOR
 #undef VECTOR_NULL
 
-extern void __stack_high__;
+extern void __stack_high;
 
 // Interrupt vectors table
-__attribute__ ((section(".isr_vector")))
+__attribute__ ((section(".vector")))
 void (* const __vectors[])(void) = {
-	&__stack_high__,
+	&__stack_high,
 
 #define VECTOR(n) _isr_##n,
 #define VECTOR_NULL NULL
