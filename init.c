9b4d05a1be5bae017f15959d7639377fc36d3546
Cody P Schafer <devel@codyps.com>
2014-07-19 16:31:51 -0700

    init tweaks

--- init.c
+++ init.c
@@ -1,23 +1,45 @@
 
-/* 0x0000.0000 to 0x0001.FFFF : Flash
- * 0x2000.0000 to 0x2000.FFFF : SRAM
- * 0x2200.0000 to 0x221F.FFFF : Bitband of SRAM
- */
-
 #include <stdint.h>
 __attribute__((noreturn))
 extern void main (void);
 
-extern uint32_t __bss_start[], __bss_end[], __data_start[], __data_end[], __data_load_start[];
+extern void __libc_init_array(void);
 
-typedef void (*init_fn)(void);
-extern init_fn __preinit_array_start[], __preinit_array_end[], __init_array_start[], __init_array_end[];
+/* make __libc_init_array() happy */
+__attribute__((weak))
+void _init(void)
+{}
+
+#if 0
+static void __libc_init_array(void)
+{
+	typedef void (*init_fn)(void);
+	extern init_fn __preinit_array_start[], __preinit_array_end[],
+	       __init_array_start[], __init_array_end[];
+
+	init_fn *f;
+	f = __preinit_array_start;
+	while (f < __preinit_array_end) {
+		(*f)();
+		f++;
+	}
+
+	_init();
+
+	f = __init_array_start;
+	while (f < __init_array_end) {
+		(*f)();
+		f++;
+	}
+}
+#endif
 
 __attribute__((noreturn,interrupt))
 void __init(void)
 {
+	extern uint32_t __bss_start[], __bss_end[], __data_start[],
+	       __data_end[], __data_load_start[];
 	uint32_t *s, *d;
-	init_fn *f;
 
 	s = __data_load_start;
 	d = __data_start;
@@ -28,17 +50,6 @@ void __init(void)
 	while (d < __bss_end)
 		*d++ = 0;
 
-	f = __preinit_array_start;
-	while (f < __preinit_array_end) {
-		(*f)();
-		f++;
-	}
-
-	f = __init_array_start;
-	while (f < __init_array_end) {
-		(*f)();
-		f++;
-	}
-
+	__libc_init_array();
 	main();
 }
