58c565e0781decb6a62cc036130924c7656a2ce2
Cody P Schafer <dev@codyps.com>
2014-08-12 03:07:20 +0000

    rework vectors to be verifiable

--- init_vector.c
+++ init_vector.c
@@ -6,25 +6,57 @@ static void bad_interrupt(void)
 {
 	__builtin_trap();
 }
-
-#define IS_reset_EQ_reset 1
-#define VECTOR(n) __attribute__((weak,interrupt,alias("bad_interrupt"))) void _isr_##n(void);
-#define VECTOR_NULL
+/*
+ * VECTOR(addres, name)
+ * VECTOR_NULL
+ * IRQ(address, name)
+ * IRQ_START_ADDR(addr)
+ * VECTOR_END(addr, vect_ct, irq_ct)
+ */
+ 
+#define VECTOR(addr, name) __attribute__((weak,interrupt,alias("bad_interrupt"))) void isr_##name(void);
+#define VECTOR_NULL(addr)
+#define IRQ(addr, name) VECTOR(addr, name)
+#define IRQ_START_ADDR(addr)
+#define VECTOR_END(addr, vect_ct, irq_ct)
 #include INCLUDE_VECTOR()
 #undef VECTOR
 #undef VECTOR_NULL
+#undef IRQ
+#undef IRQ_START_ADDR
+#undef VECTOR_END
 
 extern uint32_t __stack_high[];
 
 typedef void (isr_fn)(void);
-__attribute__ ((section(".isr_vector"),externally_visible))
-// void (* const vectors[])(void) = {
-isr_fn *const vectors[] = {
-	(isr_fn *)&__stack_high,
-#define VECTOR(n) _isr_##n,
-#define VECTOR_NULL (isr_fn *)NULL,
+struct isr_vectors_s {
+	void *stack_high;
+#define VECTOR(addr, name) isr_fn *isr_##name;
+#define VECTOR_NULL(addr) uint32_t null_at_##addr;
+#define IRQ(addr, name) VECTOR(addr, name)
+#define IRQ_START_ADDR(addr)
+#define VECTOR_END(addr, vect_ct, irq_ct)
 #include INCLUDE_VECTOR()
 #undef VECTOR
 #undef VECTOR_NULL
+#undef IRQ
+#undef IRQ_START_ADDR
+#undef VECTOR_END
+} isr_vectors;
+
+__attribute__ ((section(".isr_vector"),externally_visible))
+const struct isr_vectors_s vectors = {
+	&__stack_high,
+#define VECTOR(addr, name) &isr_##name,
+#define VECTOR_NULL(addr) 0,
+#define IRQ(addr, name) VECTOR(addr, name)
+#define IRQ_START_ADDR(addr)
+#define VECTOR_END(addr, vect_ct, irq_ct)
+#include INCLUDE_VECTOR()
+#undef VECTOR
+#undef VECTOR_NULL
+#undef IRQ
+#undef IRQ_START_ADDR
+#undef VECTOR_END
 };
 
---
+++ mk20dx128vlh5.c
@@ -0,0 +1,22 @@
+/* MK20DX128VLH5
+ * M = main
+ * K20 = k20 family
+ * D = cortex-m3 + dsp, no fpu
+ * X = flash + flex
+ * 128 = 128KB flash
+ * () = main revision
+ * V = -40 to 105 deg C
+ * LH = 64LQFP
+ * 5 = 50MHz max clock freq
+ */
+
+
+/* from http://cache.freescale.com/files/32bit/doc/ref_manual/K20P64M50SF0RM.pdf */
+
+#define KIBI(x) ((x) * 1024)
+
+#define GPIO_CT 40
+#define EEPROM_BYTES KIBI(2)
+#define SRAM_BYTES   KIBI(16)
+#define FLASH_BYTES  KIBI(128)
+#define TOTAL_FLASH_BYTES KIBI(160) /* ??? something with Flex? */
