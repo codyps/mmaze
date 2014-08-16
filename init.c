ce8c1a0064d9bfec30d7f8f4adc798625bca898f
Cody P Schafer <dev@codyps.com>
2014-08-15 23:06:11 -0400

    tup: bin objects, alias

--- init.c
+++ init.c
@@ -36,7 +36,7 @@ static void __libc_init_array(void)
 
 void __init(void);
 
-__attribute__((noreturn,interrupt))
+__attribute__((noreturn,interrupt,alias("isr_reset")))
 void __init(void)
 {
 	extern uint32_t __bss_start[], __bss_end[], __data_start[],
