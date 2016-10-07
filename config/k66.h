07589606790c131fa30d63e418340529e72b295c
Cody P Schafer <dev@codyps.com>
2016-10-07 01:05:37 -0400

    k66 vector def

---
+++ config/k66.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ARCH armv7m
+#define CORE cortex-m4
+#define INCLUDE_VECTOR() <config/k66-vector.def>
---
+++ config/teensy3_6.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/* mk66fx1m0vmd18 */
+#include <config/k66.h>
