0254269b65b666bfab7b31920033f54ba11074c2
Cody P Schafer <dev@codyps.com>
2016-10-08 21:51:12 -0400

    notes

--- config/k66.h
+++ config/k66.h
@@ -3,3 +3,18 @@
 #define ARCH armv7m
 #define CORE cortex-m4
 #define INCLUDE_VECTOR() <config/k66-vector.def>
+
+/*
+ * (board)
+ * External clocks avaliable:
+ * EXTAL0
+ * XTAL0
+ * EXTAL32
+ * XTAL32
+ */
+
+/*
+ * (app)
+ * Whole bunch of internal clocks configurable
+ *
+ */
